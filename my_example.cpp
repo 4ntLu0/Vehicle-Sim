6f53cab904af7cc221583f0731069cdcfde7d3b2
Anthony <anthonyluo100@Gmail.com>
2021-01-23 16:57:28 -0500

    gitignore

--- my_example.cpp
+++ my_example.cpp
@@ -13,17 +13,30 @@
 // a Chrono::Engine simulator with 3D view.
 // =============================================================================
 
+
+//system includes
+#include <memory>
+#include <iostream>
+
+//chrono includes
 #include "chrono/physics/ChSystemNSC.h"
 #include "chrono/physics/ChBodyEasy.h"
 #include "chrono/physics/ChLinkMate.h"
 #include "chrono/assets/ChTexture.h"
 #include "chrono/assets/ChColorAsset.h"
+
+#include "chrono/core/ChRealtimeStep.h"
+#include "chrono/physics/ChLinkDistance.h"
+#include "chrono/physics/ChLinkMotorRotationTorque.h"
+#include "chrono/physics/ChSystemNSC.h"
+
 #include "chrono_irrlicht/ChIrrApp.h"
 
 // Use the namespace of Chrono
 
 using namespace chrono;
 using namespace chrono::irrlicht;
+using namespace chrono::vehicle;
 
 // Use the main namespaces of Irrlicht
 
@@ -34,6 +47,72 @@ using namespace irr::video;
 using namespace irr::io;
 using namespace irr::gui;
 
+class SimpleCar {
+public:
+	double throttle;
+	double conic_tau;
+	double gar_tau;
+	double max_motor_torque;
+	double max_motor_speed;
+
+	// .. chassis:
+	std::shared_ptr<ChBody> chassis;
+	// .. right front suspension:
+	std::shared_ptr<ChBody> spindleRF;
+	std::shared_ptr<ChBody> wheelRF;
+	std::shared_ptr<ChLinkLockRevolute> link_revoluteRF;
+	std::shared_ptr<ChLinkDistance> link_distRFU1;
+	std::shared_ptr<ChLinkDistance> link_distRFU2;
+	std::shared_ptr<ChLinkDistance> link_distRFL1;
+	std::shared_ptr<ChLinkDistance> link_distRFL2;
+	std::shared_ptr<ChLinkDistance> link_distRSTEER;
+	std::shared_ptr<ChLinkTSDA> link_springRF;
+	// .. left front suspension:
+	std::shared_ptr<ChBody> spindleLF;
+	std::shared_ptr<ChBody> wheelLF;
+	std::shared_ptr<ChLinkLockRevolute> link_revoluteLF;
+	std::shared_ptr<ChLinkDistance> link_distLFU1;
+	std::shared_ptr<ChLinkDistance> link_distLFU2;
+	std::shared_ptr<ChLinkDistance> link_distLFL1;
+	std::shared_ptr<ChLinkDistance> link_distLFL2;
+	std::shared_ptr<ChLinkDistance> link_distLSTEER;
+	std::shared_ptr<ChLinkTSDA> link_springLF;
+	// .. right back suspension:
+	std::shared_ptr<ChBody> spindleRB;
+	std::shared_ptr<ChBody> wheelRB;
+	std::shared_ptr<ChLinkLockRevolute> link_revoluteRB;
+	std::shared_ptr<ChLinkDistance> link_distRBU1;
+	std::shared_ptr<ChLinkDistance> link_distRBU2;
+	std::shared_ptr<ChLinkDistance> link_distRBL1;
+	std::shared_ptr<ChLinkDistance> link_distRBL2;
+	std::shared_ptr<ChLinkDistance> link_distRBlat;
+	std::shared_ptr<ChLinkTSDA> link_springRB;
+	std::shared_ptr<ChLinkMotorRotationTorque> link_motorL;
+	// .. left back suspension:
+	std::shared_ptr<ChBody> spindleLB;
+	std::shared_ptr<ChBody> wheelLB;
+	std::shared_ptr<ChLinkLockRevolute> link_revoluteLB;
+	std::shared_ptr<ChLinkDistance> link_distLBU1;
+	std::shared_ptr<ChLinkDistance> link_distLBU2;
+	std::shared_ptr<ChLinkDistance> link_distLBL1;
+	std::shared_ptr<ChLinkDistance> link_distLBL2;
+	std::shared_ptr<ChLinkDistance> link_distLBlat;
+	std::shared_ptr<ChLinkTSDA> link_springLB;
+	std::shared_ptr<ChLinkMotorRotationTorque> link_motorR;
+
+	SimpleCar(ChSystemNSC &my_system, ISceneManager *msceneManager, IVideoDriver *mdriver) {
+		throttle = 0;
+		conic_tau = 0.2;
+		gear_tau = 0.3;
+		max_motor_torque = 80;
+		max_motor_speed = 800;
+
+		auto texture = chrono_types::make_shared<ChTexture>();
+		texture->SetTextureFilename(GetChronoDataFile("bluwhite.png"));
+	}
+};
+
+/* ignore this main function - we will write our own.
 int main(int argc, char* argv[]) {
     // Set path to Chrono data directory
     SetChronoDataPath(CHRONO_DATA_DIR);
@@ -139,3 +218,4 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+*/
